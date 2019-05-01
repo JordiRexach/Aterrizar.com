78c4010b356f97985a11edcb4e07c2049c2d363f
JordiRexach <jordi.rexach13@gmail.com>
2019-05-01 00:25:07 -0300

    Se agrego compatibilidad con los formatos ISO8601, Latinoamericano y Norteamericano, cumpliendo con los requisitos pedidos. Agregados los test necesarios para evaluar esto.

--- src/modelo/ConsultarFecha.java
+++ src/modelo/ConsultarFecha.java
@@ -4,18 +4,39 @@ import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+
 public class ConsultarFecha {
 	
-	public Date convertirISO8601AFecha(String ISO8601) {
-		SimpleDateFormat formatoISO8601 = new SimpleDateFormat("yyyy-MM-dd");
+	private String formatoISO8601 = "([0-9]{4})-([0-9]{2})-([0-9]{2})";
+	private String formatoLatinoamericano = "([0-9]{2})/([0-9]{2})/([0-9]{4})";
+	private String formatoNorteamericano = "([0-9]{2})-([0-9]{2})-([0-9]{4})";
+	
+	public Date convertirFecha(String fechaEnTexto) {
+		SimpleDateFormat formato = compararFecha(fechaEnTexto);
 		Date fecha = null;
 		
 		try {
-			fecha = formatoISO8601.parse(ISO8601);
+			fecha = formato.parse(fechaEnTexto);
 		} 		
 		catch (ParseException e) {
 			System.out.println(e);
 		}
 		return fecha;
 	}
+	
+	private SimpleDateFormat compararFecha(String fechaEnTexto) {
+        SimpleDateFormat formatoDeFecha = null;
+
+        if (fechaEnTexto.matches(formatoISO8601)) {
+            formatoDeFecha = new SimpleDateFormat("yyyy-MM-dd");
+        } 
+        else if (fechaEnTexto.matches(formatoLatinoamericano)) {
+            formatoDeFecha = new SimpleDateFormat("dd/MM/yyyy");
+        } 
+        else if (fechaEnTexto.matches(formatoNorteamericano)) {
+            formatoDeFecha = new SimpleDateFormat("MM-dd-yyyy");
+        }
+        
+        return formatoDeFecha;
+    }
 }
--- test/modelo/ConsultarFechaTest.java
+++ test/modelo/ConsultarFechaTest.java
@@ -1,5 +1,47 @@
 package modelo;
 
+import java.util.Calendar;
+import java.util.Date;
+import junit.framework.Assert;
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
 public class ConsultarFechaTest {
+	
+	@Test
+	public void convertirFecha_LaFechaEnFormatoISO8601SeConvierteEnDate(){
+		ConsultarFecha textoFecha = new ConsultarFecha();
+		Date fecha = textoFecha.convertirFecha("2019-04-30");
+		
+		Calendar resultadoEsperado = Calendar.getInstance();
+        resultadoEsperado.clear();
+        resultadoEsperado.set(2019, Calendar.APRIL, 30);
+        
+		Assert.assertTrue("La fecha en formato ISO8601 no se a convertido a formato Date", resultadoEsperado.getTime().equals(fecha));
+	}
+	
+	@Test
+	public void convertirFecha_LaFechaEnFormatoLatinoamericanoSeConvierteEnDate(){
+		ConsultarFecha textoFecha = new ConsultarFecha();
+		Date fecha = textoFecha.convertirFecha("30/04/2019");
+		
+		Calendar resultadoEsperado = Calendar.getInstance();
+        resultadoEsperado.clear();
+        resultadoEsperado.set(2019, Calendar.APRIL, 30);
+        
+		Assert.assertTrue("La fecha en formato Latinoamericano no se a convertido a formato Date", resultadoEsperado.getTime().equals(fecha));
+	}
+	
+	@Test
+	public void convertirFecha_LaFechaEnFormatoNorteamericanoSeConvierteEnDate(){
+		ConsultarFecha textoFecha = new ConsultarFecha();
+		Date fecha = textoFecha.convertirFecha("04-30-2019");
+		
+		Calendar resultadoEsperado = Calendar.getInstance();
+        resultadoEsperado.clear();
+        resultadoEsperado.set(2019, Calendar.APRIL, 30);
+        
+		Assert.assertTrue("La fecha en formato Norteamericano no se a convertido a formato Date", resultadoEsperado.getTime().equals(fecha));
+	}
 
 }
